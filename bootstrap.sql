e4b5441c79234200ac772a7720170a4d27f89a94
dan003400 <dan.wetherald@gmail.com>
2018-09-07 00:37:52 -0500

    Create Database and User with Password

--- bootstrap.sql
+++ bootstrap.sql
@@ -1,4 +1,4 @@
 DROP DATABASE apollo_server;
 CREATE DATABASE apollo_server;
-CREATE ROLE apollo_server;
+CREATE USER apollo_server WITH ENCRYPTED PASSWORD 'apollo_server';
 GRANT ALL PRIVILEGES ON DATABASE apollo_server TO apollo_server;
